bf3fd691403b95d34c60bb622418d576ea288a60
vimcoder <vimcoders@gmail.com>
2026-07-20 08:53:23 +0800

    generated

--- generated/generated-golang.bat
+++ generated/generated-golang.bat
@@ -6,7 +6,9 @@ buf format -w %~dp0proto
 %~dp0protoc-35.1-win64\bin\protoc ^
 --plugin=protoc-gen-go=%GOPATH%\bin\protoc-gen-go.exe ^
 --plugin=protoc-gen-go-grpc=%GOPATH%\bin\protoc-gen-go-grpc.exe ^
+--plugin=protoc-gen-grpc-gateway=%GOPATH%\bin\protoc-gen-grpc-gateway.exe ^
 --proto_path=%~dp0proto ^
 --go_out=%~dp0kubeapi ^
 --go-grpc_out=%~dp0kubeapi ^
+--grpc-gateway_out=%~dp0kubeapi ^
 %~dp0proto\*.proto
